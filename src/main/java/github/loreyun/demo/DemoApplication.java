784b1a1b79c40d0ec42e75c7154d5c665b0aeb3e
LoreYun <wc250025@gmail.com>
2018-03-06 10:16:38 +0800

    init

---
+++ src/main/java/github/loreyun/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.github.loreyun.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+/**
+ * @Author: LoreYun
+ * @Date: 2018/3/6 9:47
+ * @Description:
+ */
+@SpringBootApplication
+public class DemoApplication {
+		public static void main(String[] args) {
+			SpringApplication.run(DemoApplication.class, args);
+		}
+}
